f6c6405fffd32fce0e51d4991c6b457177308edd
dannygb <dan.beavon@education.gov.uk>
2019-11-01 09:51:47 +0000

    Initial commit of the bare coding exercises solution containing the FizzBuzz and Math problems

---
+++ Maths/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 2;
+
+            Console.WriteLine(Multiply(a, b).ToString());
+            Console.WriteLine(Divide(a, b).ToString());
+            Console.ReadKey();
+        }
+
+        private static int Multiply(int a, int b)
+        {
+            // Implement the method to multiply a with b without using the multiplication operator (*)
+            // the expected result should is 8 given a = 4 and b = 2
+            throw new NotImplementedException();
+        }
+
+        private static int Divide(int a, int b)
+        {
+            // Implement the method to divide a with b without using the division operator (/)
+            // the expected result should is 2 given a = 4 and b = 2
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Maths/READEME.md
@@ -0,0 +1,9 @@
+﻿Calc
+====
+
+This is a .Net Core Console Application, it has no external dependencies
+
+This is a coding exercise to:
+
+1. Create a program that can multiply two numbers without using the multiplication operator (* in C#)
+2. Extend the program in 1 to also be able to divide two numbers without using the division operator (/ in C#)
